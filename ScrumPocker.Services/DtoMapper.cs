073eaf4a3eeb095b9ba44cc91a135a3916814653
Adil Deveci <adildeveci@hotmail.com>
2022-02-27 00:16:57 +0300

    #19 Service katmanina automapper eklendi

---
+++ ScrumPocker.Services/DtoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ScrumPocker.Core.Dto.Room;
+using ScrumPocker.Core.Models;
+
+namespace ScrumPocker.Services
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<CreateRoomDto, Room>().ReverseMap(); 
+        }
+    }
+}
